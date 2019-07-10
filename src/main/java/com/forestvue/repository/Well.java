ff2cf65fd5601d598970430991b2d85c6318ebb2
Choi Seungeon <chou610@naver.com>
2019-07-11 08:58:03 +0900

    [Update] user locking

--- src/main/java/com/forestvue/repository/Well.java
+++ src/main/java/com/forestvue/repository/Well.java
@@ -2,14 +2,25 @@ package com.forestvue.repository;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 @Repository
 public class Well {
     private int data = 10000;
+    private Lock lock;
+    Well(){
+        lock = new ReentrantLock();
+    }
     public void remove(int amount){
+        this.lock.lock();
         data -= amount;
+        this.lock.unlock();
     }
     public void add(int amount){
+        this.lock.lock();
         data += amount;
+        this.lock.unlock();
     }
     public int getWell(){
         return data;
